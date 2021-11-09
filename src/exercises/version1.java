538d973970ef3647ec889a86d47b19b5fb8098a5
Robin <robin.hermann@rub.de>
2021-11-09 09:48:20 +0100

    version 1 finished

---
+++ src/exercises/version1.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class version1 {
+
+    public static void main(String[] args) {
+        // Version 1 is not implementable because Java does not contain an executable goto statement.
+    }
+}
